f57ce25458438348a0654f798e2cb8b8ad64bfb1
Abhishekrathor1234321 <abhishekrathor7447@gmail.com>
2026-03-20 04:01:45 +0530

    Fixed navbar jobs button and popup styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -119,11 +119,11 @@ function Navbar() {
             />
 
             {/* Popup Card */}
-          <motion.div
-  initial={{ opacity: 0, scale: 0.85 }}
+      <motion.div
+  initial={{ opacity: 0, scale: 0.9 }}
   animate={{ opacity: 1, scale: 1 }}
-  exit={{ opacity: 0, scale: 0.85 }}
-  transition={{ type: "spring", stiffness: 300, damping: 25 }}
+  exit={{ opacity: 0, scale: 0.9 }}
+  transition={{ duration: 0.2 }}
   className="jobs-popup"
 >
               {/* Close */}
