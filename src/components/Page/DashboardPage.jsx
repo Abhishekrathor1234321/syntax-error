1a5d6cf3a0a30821dc01eabff82460565198a624
Abhishekrathor1234321 <abhishekrathor7447@gmail.com>
2026-03-31 16:31:32 +0530

    DashboardPage update

--- src/components/Page/DashboardPage.jsx
+++ src/components/Page/DashboardPage.jsx
@@ -268,8 +268,8 @@ function DashboardPage() {
                 </div>
               ) : (
                 <p className="dash-empty">
-                  Abhi koi notes download nahi kiye!
-                  <span onClick={() => navigate("/notes")}> Notes dekho →</span>
+                  You haven’t downloaded any notes yet!
+                  <span onClick={() => navigate("/notes")}> View Notes →</span>
                 </p>
               )}
             </div>
