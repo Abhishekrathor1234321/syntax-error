051a6c2e764f10f2c84711203f87395a2339a89c
Abhishekrathor1234321 <abhishekrathor7447@gmail.com>
2026-05-17 19:53:48 +0530

    TCSCourseDetail Update

--- src/components/Page/TCSCourseDetail.jsx
+++ src/components/Page/TCSCourseDetail.jsx
@@ -265,7 +265,7 @@ rzp.on('payment.dismiss', async function() {
         <CheckoutModal
           course={{
             title: "The Complete TCS NQT Course 2026",
-            amount: 1,
+            amount: 999,
             coupons: {},
           }}
           onClose={() => setShowCheckout(false)}
